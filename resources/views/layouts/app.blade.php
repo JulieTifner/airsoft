a56f490503b0b8bedf534f0c32785d064abcee7c
Julie Tifner <tifnerjulie@lt005-ws-zos.ikhost.ch>
2023-05-24 16:13:38 +0200

    aut

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,43 @@
-
 <!DOCTYPE html>
 <html>
-    <head>
-        @vite('resources/css/app.css')
-        @yield('css')
-        
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title></title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.1/css/all.css">
+
+    <!-- Styles -->
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
+    @yield('css')
+
+</head>
+
+<body>
+    <div id="app">
+
         @include('layouts.include.navbar')
-    </head>
-    <body>
-        <div id="app">
+        
+        <div class="container d-flex flex-column maximus" style="min-height: calc(100vh - 374px);">
             @yield('content')
-
-            {{-- @include('layouts.include.footer') --}}
-
         </div>
 
-    </body>
+        
+    </div>
+</body>
 </html>
