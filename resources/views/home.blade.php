19d235e003ab777f42be33de19b7997f1b27730f
Julie Tifner <tifnerjulie@lt005-ws-zos.ikhost.ch>
2023-05-26 16:22:52 +0200

    homepage diashow

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,37 @@
         sed diam nonumy eirmod tempor invidunt ut labore et dolore
         magna aliquy</p> 
     
+        <div class="btn-group">
+          <a href="#">Gallerie</a>
+        </div>
     </div>
-    <div class="box">
+ 
+    <div class="section">
+      
     </div>
   </div>
+
+<script>
+  console.log('huisbd');
+  // Hintergrundbilder für die Diashow
+  var backgroundImages = [
+    'https://images.hdqwalls.com/wallpapers/call-of-duty-mobile-4k-game-2019-3a.jpg',
+    'https://c4.wallpaperflare.com/wallpaper/962/231/806/airsoft-wallpaper-preview.jpg',
+    'https://img.freepik.com/premium-photo/portrait-airsoft-player-professional-equipment-with-machine-gun-abandoned-ruined-building-soldier-with-weapons-war-smoke-fog_154092-2407.jpg'
+  ];
+  
+  // Index des aktuellen Hintergrundbilds
+  var currentIndex = 0;
+  
+  // Funktion zum Ändern des Hintergrundbilds
+  function changeBackgroundImage() {
+    var section = document.querySelector('.wrapper');
+    section.style.backgroundImage = 'url(' + backgroundImages[currentIndex] + ')';
+    currentIndex = (currentIndex + 1) % backgroundImages.length;
+  }
+  
+  // Initialisierung der Diashow
+  changeBackgroundImage();
+  setInterval(changeBackgroundImage, 5000); // Hintergrundbild alle 5 Sekunden ändern
+</script>
 @endsection
