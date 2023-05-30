7e98467581f8fb83f422b5158ad94a26ab7a355f
JulieTifner <julie@tifner.ch>
2023-05-30 17:08:55 +0200

    calendar style

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,10 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="calendar-wrapper">
 <div class="container">
     <div id='calendar'></div>
 </div>
    
+<div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="eventModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="eventModalLabel">Event Title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- Form to add event details -->
+                <form id="eventForm">
+                    <div class="form-group">
+                        <label for="eventTitle">Event Title</label>
+                        <input type="text" class="form-control" id="eventTitle" name="title">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="saveEvent">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
 <script>
 $(document).ready(function () {
    
@@ -31,8 +60,8 @@ var calendar = $('#calendar').fullCalendar({
                     selectable: true,
                     selectHelper: true,
                     select: function (start, end, allDay) {
-                        var title = prompt('Event Title:');
-                        if (title) {
+                        $('#eventModal').modal('show');
+                         if (title) {
                             var start = $.fullCalendar.formatDate(start, "Y-MM-DD");
                             var end = $.fullCalendar.formatDate(end, "Y-MM-DD");
                             $.ajax({
@@ -99,6 +128,8 @@ var calendar = $('#calendar').fullCalendar({
                     }
  
                 });
+                $('#eventModal').modal('hide');
+
  
 });
  
